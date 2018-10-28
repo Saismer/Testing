629f637e0f7857901f9f86f95ee7b69cc87ee811
saismer <alesha_ryzhkovski@mail.ru>
2018-10-28 19:34:21 +0300

    PageFactory lab

---
+++ PageFactory/Driver/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+
+namespace Driver
+{
+    class Program
+    {
+        public static IWebDriver browser;
+
+        static void Main(string[] args)
+        {
+
+            browser = new OpenQA.Selenium.Chrome.ChromeDriver();
+            browser.Manage().Window.Maximize();
+            browser.Navigate().GoToUrl("https://www.anywayanyday.com/en/");
+            WebPageFactory page = new WebPageFactory();
+
+            page.cityFrom.SendKeys("Moscow" + OpenQA.Selenium.Keys.Enter);
+            System.Threading.Thread.Sleep(2000);
+
+            page.cityTo.SendKeys("Moscow" + OpenQA.Selenium.Keys.Enter);
+
+            page.date.Click();
+            System.Threading.Thread.Sleep(2000);
+
+            page.datePick.Click();
+            System.Threading.Thread.Sleep(2000);
+
+            page.searchFlight.Click();
+            System.Threading.Thread.Sleep(2000);      
+                 
+            page.editMode.Click();
+            System.Threading.Thread.Sleep(2000);
+
+            page.edit.Click();
+            System.Threading.Thread.Sleep(2000);
+
+            page.cityFrom.Clear();
+            page.cityFrom.SendKeys("Minsk");
+            page.focus.Click();
+            System.Threading.Thread.Sleep(2000);
+
+            page.searchRightFlight.Click();
+            System.Threading.Thread.Sleep(20000);
+            browser.Quit();
+        }
+    }
+}
---
+++ PageFactory/Driver/WebPageFactory.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+
+namespace Driver
+{
+    class WebPageFactory
+    {
+        public WebPageFactory()
+        {
+            PageFactory.InitElements(Program.browser, this);
+        }
+
+        [FindsBy(How = How.ClassName, Using = "awadWidget-aviaInput")]
+        public IWebElement cityFrom;
+
+        [FindsBy(How = How.XPath, Using = "//*[@id=\"sidebar\"]/div/div/div/div/div[2]/div[3]/div/div[2]/div/input")]
+        
+        public IWebElement cityTo;
+
+        [FindsBy(How = How.XPath, Using = "//*[@id=\"sidebar\"]/div/div/div/div/div[2]/div[3]/div/div[4]/div/div[1]")]
+        public IWebElement date;
+
+        [FindsBy(How = How.XPath, Using = "//*[@id=\"sidebar\"]/div/div[1]/div/div/div[2]/div[3]/div/div[5]/div/div[2]/div[2]/table/tbody/tr[5]/td[5]/span")]
+        public IWebElement datePick;
+
+        [FindsBy(How = How.XPath, Using = "//*[@id=\"sidebar\"]/div/div/div/div/div[2]/div[4]/a")]
+        public IWebElement searchFlight;
+
+        [FindsBy(How = How.XPath, Using = "//*[@id=\"sidebar\"]/div/div/div/div/div[2]/div[2]/ul/li/div[1]/div[1]/div[2]")]
+        public IWebElement editMode;
+
+        [FindsBy(How = How.XPath, Using = "//*[@id=\"sidebar\"]/div/div/div/div/div[2]/div[2]/ul/li/div[1]/div[2]/div[1]")]
+        public IWebElement edit;
+
+        [FindsBy(How = How.XPath, Using = "//*[@id=\"sidebar\"]/div/div/div/div/div[2]/div[4]/a")]
+        public IWebElement searchRightFlight;
+
+        [FindsBy(How = How.XPath, Using = "//*[@id=\"page\"]/div[2]/div[1]/div")]
+        public IWebElement focus;
+    }
+}
