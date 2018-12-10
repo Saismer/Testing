db3a87bf00717e42c64e3d645c2e2663733e1a69
saismer <alesha_ryzhkovski@mail.ru>
2018-12-10 13:27:14 +0300

    Fixed broken calendar

--- Driver/Driver/WebDriverTest.cs
+++ Driver/Driver/WebDriverTest.cs
@@ -4,12 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using OpenQA.Selenium;
+using NUnit.Framework;
 
 namespace Driver
 {
-    class Program
+    [TestFixture]
+    class TestWebDriver
     {
-        static void Main(string[] args)
+
+        [Test]
+        public void Main()
         {
             IWebDriver browser;
             browser = new OpenQA.Selenium.Chrome.ChromeDriver();
@@ -27,21 +31,28 @@ namespace Driver
             date.Click();
             System.Threading.Thread.Sleep(2000);
 
-            IWebElement datePick = browser.FindElement(By.XPath("//*[@id=\"sidebar\"]/div/div[1]/div/div/div[2]/div[3]/div/div[5]/div/div[2]/div[2]/table/tbody/tr[5]/td[6]/span"));
+            IWebElement datePick = browser.FindElement(By.XPath("//*[@id=\"sidebar\"]/div/div/div/div/div[2]/div[3]/div/div[5]/div/div[2]/div[2]/table/tbody/tr[5]/td[4]/span"));
             datePick.Click();
             System.Threading.Thread.Sleep(2000);
 
             IWebElement returnFlight = browser.FindElement(By.XPath("//*[@id=\"sidebar\"]/div/div[1]/div/div/div[2]/div[4]/div[1]/div[1]"));
             returnFlight.Click();
             System.Threading.Thread.Sleep(2000);
+            try
+            {
+                IWebElement datePickSecond = browser.FindElement(By.XPath("//*[@id=\"sidebar\"]/div/div[1]/div/div/div[2]/div[3]/div/div[5]/div/div[2]/div[2]/table/tbody/tr[5]/td[2]/span"));
+                datePickSecond.Click();
+                System.Threading.Thread.Sleep(2000);
+                Assert.AreNotEqual(null, browser.FindElement(By.XPath("//*[@id=\"sidebar\"]/div/div[1]/div/div/div[2]/div[2]/ul/li[2]/div[1]/div[1]/div[1]/div[1]")));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message.ToString());
+            }
 
-            IWebElement datePickSecond = browser.FindElement(By.XPath("//*[@id=\"sidebar\"]/div/div[1]/div/div/div[2]/div[3]/div/div[5]/div/div[2]/div[2]/table/tbody/tr[5]/td[2]/span"));
-            datePickSecond.Click();
-            System.Threading.Thread.Sleep(2000);
-         
             IWebElement searchFlight = browser.FindElement(By.XPath("//*[@id=\"sidebar\"]/div/div/div/div/div[2]/div[4]/a"));
             searchFlight.Click();
-            System.Threading.Thread.Sleep(2000);      
+            System.Threading.Thread.Sleep(2000);
         }
     }
 }
